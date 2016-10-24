e673781d4905336050777823ade50e754c5d6126
Sergio Cáceres <sergiocaceres@correo.ugr.es>
2016-10-25 01:44:29 +0200

    Cambiando test.py

--- empresa/tests.py
+++ empresa/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
-# Create your tests here.
+class Test(TestCase):
+	def test_form(self):
+		c = Post(nombre_empresa = 'EmpresaTest', persona = 'PersonaTest', fecha = timezone.now(), puntuacion = '9')
+		c.save()
+		self.assertEqual(c.nombre_empresa, "EmpresaTest")
